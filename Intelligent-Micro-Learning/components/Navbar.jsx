ae16f649259cfd12ba57ed8ff5a4d9f6f70cc761
Dhruvin151104 <131797669+Dhruvin151104@users.noreply.github.com>
2024-04-21 02:17:29 +0530

    Added login part

--- Intelligent-Micro-Learning/components/Navbar.jsx
+++ Intelligent-Micro-Learning/components/Navbar.jsx
@@ -1,27 +1,60 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Button } from './ui/button'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Navbar() {
+  const { loginWithRedirect, user, isAuthenticated, logout } = useAuth0();
+
   return (
-    <div className='flex gap-10 justify-center items-center h-[10vh] w-full'>
-        <Link to='/videos'>
-      <Button variant='ghost' size='lg'>
-        Videos
-      </Button>
+    <div className="flex justify-between items-center h-[10vh] w-full px-10">
+      <div className="w-[80%] h-full flex">
+        <div className="w-[40%] h-full flex justify-start items-center text-4xl font-medium bg-gradient-to-r from-amber-300 via-amber-500 to-orange-600 text-transparent bg-clip-text">
+        Eduverse
+        </div>
+        <div className="w-[60%] h-full flex gap-10 justify-end items-center">
+        <Link to="/videos">
+          <Button variant="ghost" size="lg">
+            Videos
+          </Button>
         </Link>
-        <Link to='/recommendations'>
-      <Button variant='ghost' size='lg'>
-        Recommendations
-      </Button>
+        <Link to="/recommendations">
+          <Button variant="ghost" size="lg">
+            Recommendations
+          </Button>
         </Link>
-        <Link to='/quiz'>
-      <Button variant='ghost' size='lg'>
-        Quiz
-      </Button>
+        <Link to="/quiz">
+          <Button variant="ghost" size="lg">
+            Quiz
+          </Button>
         </Link>
+        </div>
+      </div>
+      <div className="h-full w-[20%] flex items-center justify-end">
+        {!isAuthenticated ? (
+          <Button
+            variant="ghost"
+            size="lg"
+            className="bg-amber-500"
+            onClick={() => loginWithRedirect()}
+          >
+            Log In
+          </Button>
+        ) : (
+          <Button
+            className="bg-amber-500"
+            variant="ghost"
+            size="lg"
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            Log Out
+          </Button>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- Intelligent-Micro-Learning/src/main.jsx
+++ Intelligent-Micro-Learning/src/main.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Auth0Provider
+    domain="dev-l0zajyj3k1662cbp.us.auth0.com"
+    clientId="iq6ZxjSqZeSnJXJ92LDn4oIoFLMQbMpz"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </Auth0Provider>
+);
