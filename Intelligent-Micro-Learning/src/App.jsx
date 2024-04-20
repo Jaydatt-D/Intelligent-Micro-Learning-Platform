63d0bb05ac9b51ae6fd8d14f6167edfd51f276c9
Dhruvin151104 <131797669+Dhruvin151104@users.noreply.github.com>
2024-04-20 17:59:10 +0530

    Added Frameworks

---
+++ Intelligent-Micro-Learning/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from '../components/ui/button'
+import React from 'react'
+
+function App() {
+  return (
+    <div className='flex justify-center items-center min-h-screen'>
+      <Button>Hellooo</Button>
+    </div>
+  )
+}
+
+export default App
